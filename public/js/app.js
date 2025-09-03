1444c5cc0b81d1dc2989bf0e16ad964db1bf2670
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-03 20:28:34 +0700

    Create app.js

---
+++ public/js/app.js
@@ -0,0 +1,3 @@
+// small helper functions
+function toastSuccess(msg){ Swal.fire({icon:'success', title: msg, toast:true, position:'top-end', timer:1500, showConfirmButton:false}); }
+function toastError(msg){ Swal.fire({icon:'error', title: msg, toast:true, position:'top-end', timer:2000, showConfirmButton:false}); }
